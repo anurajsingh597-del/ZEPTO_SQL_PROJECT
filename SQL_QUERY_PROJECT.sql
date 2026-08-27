b0b1a80611d2fe7d6b41b85b4826e5260280659f
anurajsingh597-del <anurajsingh597@gmail.com>
2026-08-28 00:36:01 +0530

    Added category-wise average discount query

--- SQL_QUERY_PROJECT.sql
+++ SQL_QUERY_PROJECT.sql
@@ -91,20 +91,26 @@ from zepto
 where outofstock = true and mrp>300
 order by mrp desc;
 
---##Q3.Calculate Estimated Revenue for each category
+--##Q3. Category-wise Average Discount
+select category, round(avg(discountpercent),2) as avg_discount
+from zepto
+group by category
+order by avg_discount desc;
+
+--##Q4.Calculate Estimated Revenue for each category
 select category,
 sum(discountedsellingprice * availablequantity) as total_revenue
 from zepto
 group by category
 order by total_revenue;
 
--- ##Q4. Find all products where MRP is greater than ₹500 and discount is less than 10%.
+-- ##Q5. Find all products where MRP is greater than ₹500 and discount is less than 10%.
 select distinct name,mrp,discountpercent
 from zepto 
 where mrp >500 and discountpercent< 10
 order by mrp desc,discountpercent desc;
 
--- Q5. Identify the top 5 categories offering the highest average discount percentage.
+-- Q6. Identify the top 5 categories offering the highest average discount percentage.
 select category,
 round(AVG(discountpercent),2) as avg_discount
 from zepto
@@ -112,14 +118,14 @@ group by category
 order by avg_discount desc
 limit 5;
 
--- ##Q6. Find the price per gram for products above 100g and sort by best value.
+-- ##Q7. Find the price per gram for products above 100g and sort by best value.
 select distinct name,weightingms,discountpercent,
 round(discountedsellingprice/weightingms,2) as price_per_gram
 from zepto
 where weightingms >= 100
 order by price_per_gram;
 
---Q7.Group the products into categories like Low, Medium, Bulk.
+--Q8.Group the products into categories like Low, Medium, Bulk.
 select distinct name,weightingms,
 case when weightingms < 1000 then 'low'
 when weightingms < 5000 then 'medium'
@@ -127,7 +133,7 @@ else 'bulk'
 end as weight_category
 from zepto;
 
---##Q8.What is the Total Inventory Weight Per Category 
+--##Q9.What is the Total Inventory Weight Per Category 
 select category,
 sum(weightingms*availablequantity)as total_weight
 from zepto
